d66a127f53e136543a66f87b788075c2b4bb13b2
TStrothjohann <thomas@strothjohann.net>
2016-11-10 18:49:20 +0100

    Added device lookup, has-tag, channel lookup, active-channel-lookup

--- app/models/Tag.rb
+++ app/models/Tag.rb
@@ -2,7 +2,7 @@ require "urbanairship"
 UA = Urbanairship
 
 class Tag
-  attr_accessor :name, :named_user
+  attr_accessor :name, :named_user, :tag_group_name
   
   def initialize
     @airship = UA::Client.new(key: ENV['ua_key'], secret: ENV['ua_master_secret'])
@@ -15,11 +15,43 @@ class Tag
     return user['body']['named_user']['tags']
   end
 
+  def ua_get_user_info
+    ua_named_user = UA::NamedUser.new(client: @airship)
+    ua_named_user.named_user_id = @named_user
+    user = ua_named_user.lookup
+    return user
+  end
+
   def ua_add_tag
     named_user_tags = UA::NamedUserTags.new(client: @airship)
     named_user_ids = [@named_user] 
     named_user_tags.set_audience(user_ids: named_user_ids)
-    named_user_tags.add(group_name: 'subscriptions', tags: [@name])
+    named_user_tags.add(group_name: @tag_group_name, tags: [@name])
     named_user_tags.send_request
+  end
+  
+  def ua_has_tag
+    ua_named_user = UA::NamedUser.new(client: @airship)
+    ua_named_user.named_user_id = @named_user
+    begin
+      user = ua_named_user.lookup
+      tags = user['body']['named_user']['tags'][@tag_group_name]
+    rescue
+      tags = []
+    end
+    
+    return tags.include? @name
+  end
+
+  def ua_active_channels
+    active_channels = []
+    ua_named_user = UA::NamedUser.new(client: @airship)
+    ua_named_user.named_user_id = @named_user
+    user = ua_named_user.lookup
+    channels = user['body']['named_user']['channels']
+    channels.each do |chn|
+      active_channels.push(chn['device_type']) if chn['installed'] && chn['opt_in']
+    end
+    return active_channels
   end
 end
--- app/push_subscriber.rb
+++ app/push_subscriber.rb
@@ -5,21 +5,47 @@ require 'dotenv'
 
 class PushSubscriber < Sinatra::Base
   register Sinatra::Partial
-
+  
+  before do
+    response['Access-Control-Allow-Origin'] = @cors_domain = "http://friedbert-preview.zeit.de"
+  end
+  
   get '/' do
     return json :hello => "World"
   end
 
-  get '/add-tag' do
+  get '/user-info' do
     mytag = Tag.new
-    mytag.name = params['name']
     mytag.named_user = params['ssoid']
+    json mytag.ua_get_user_info
+  end
+
+  post '/add-tag' do
+    mytag = Tag.new
+    mytag.name = params['tag_name']
+    mytag.named_user = params['ssoid']
+    mytag.tag_group_name = params['tag_group_name']
     json mytag.ua_add_tag
   end
 
-  get '/get-tags' do
+  post '/get-tags' do
     mytag = Tag.new
     mytag.named_user = params['ssoid']
     json mytag.ua_get_info    
   end
+
+  get '/has-tag' do
+    mytag = Tag.new
+    mytag.name = params['tag_name']
+    mytag.named_user = params['ssoid']
+    mytag.tag_group_name = params['tag_group_name']
+    json :has_tag => mytag.ua_has_tag 
+  end
+
+  get '/active-channels' do
+    mytag = Tag.new
+    mytag.named_user = params['ssoid']    
+    json :active_channels => mytag.ua_active_channels
+  end
+
 end
